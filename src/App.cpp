d70dfdb5620c2132d4f5f9e530adefb4adbe1581
YlmzCmlttn <ylmzcmlttn@gmail.com>
2025-02-02 23:50:37 +0300

    Index and Staging Buffers

--- src/App.cpp
+++ src/App.cpp
@@ -8,9 +8,10 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
-
+#include <chrono>
 #include <glm/gtc/constants.hpp>
 #include "SimpleRenderSystem.hpp"
+#include "KeyboardMovementController.hpp"
 #include "Camera.hpp"
 namespace learnVulkan{
     App::App() {        
@@ -24,8 +25,25 @@ namespace learnVulkan{
         SimpleRenderSystem simpleRenderSystem{m_Device,m_Renderer.getSwapChainRenderPass()};
         Camera camera{};
         camera.setViewTarget(glm::vec3(-1.f, -2.f, -2.f), glm::vec3(0.f, 0.f, 2.5f));
+
+        auto viewerObject = GameObject::createGameObject();
+        KeyboardMovementController cameraController{};
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+
         while (!m_Window.shouldClose()) {
             glfwPollEvents();
+
+            auto newTime = std::chrono::high_resolution_clock::now();
+            float frameTime =
+                std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+            currentTime = newTime;
+
+            cameraController.moveInPlaneXZ(m_Window.getGLFWwindow(), frameTime, viewerObject);
+            camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
+
+
+
             float aspect = m_Renderer.getAspectRatio();
             camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
 
@@ -40,58 +58,105 @@ namespace learnVulkan{
         vkDeviceWaitIdle(m_Device.device()); //CPU block untill everything finished;
     }
 
-    
-    std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset){
-        
-        std::vector<Model::Vertex> vertices{            
-        // left face (white)
-        {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-        {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-        {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-        {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-        {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-        {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-        // right face (yellow)
-        {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-        {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-        {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-        {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-        {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-        {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-        // top face (orange, remember y axis points down)
-        {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-        {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-        {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-        {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-        {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-        {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-        // bottom face (red)
-        {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-        {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-        {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-        {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-        {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-        {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-        // nose face (blue)
-        {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-        {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-        {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-        {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-        {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-        {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-        // tail face (green)
-        {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-        {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-        {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-        {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-        {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-        {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
+        Model::Builder modelBuilder{};
+        modelBuilder.vertices = {
+            // left face (white)
+            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+            {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+            {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+
+            // right face (yellow)
+            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+            {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+            {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+
+            // top face (orange, remember y axis points down)
+            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+            {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+            {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+
+            // bottom face (red)
+            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+            {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+            {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+
+            // nose face (blue)
+            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+            {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+            {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+
+            // tail face (green)
+            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+            {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+            {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
         };
-        for (auto& v : vertices) {
+        for (auto& v : modelBuilder.vertices) {
             v.position += offset;
         }
-        return std::make_unique<Model>(device, vertices);
-    }
+
+        modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
+                                12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
+
+        return std::make_unique<Model>(device, modelBuilder);
+        }
+    // std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset){
+        
+    //     std::vector<Model::Vertex> vertices{            
+    //     // left face (white)
+    //     {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+    //     {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+    //     {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+    //     {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+    //     {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+    //     {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+    //     // right face (yellow)
+    //     {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+    //     {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+    //     {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+    //     {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+    //     {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+    //     {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+    //     // top face (orange, remember y axis points down)
+    //     {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    //     {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    //     {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    //     {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    //     {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    //     {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    //     // bottom face (red)
+    //     {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    //     {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    //     {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    //     {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    //     {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    //     {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    //     // nose face (blue)
+    //     {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    //     {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    //     {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    //     {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    //     {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    //     {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    //     // tail face (green)
+    //     {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    //     {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    //     {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    //     {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    //     {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    //     {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    //     };
+    //     for (auto& v : vertices) {
+    //         v.position += offset;
+    //     }
+    //     return std::make_unique<Model>(device, vertices);
+    // }
     void App::loadGameObjects() {
         std::shared_ptr<Model> model = createCubeModel(m_Device, {.0f, .0f, .0f});
         auto cube = GameObject::createGameObject();
